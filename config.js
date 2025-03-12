61071186c5bbfacc07f88e366b0e0319046fcbf3
dttaivlo1 <76785450+dttaivlo1@users.noreply.github.com>
2025-03-12 16:08:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Kiều à!',
     introDesc: `Chào người đẹp của anh nhé`,
-    btnIntro: 'Người đẹp chào anh nhé',
-    title: 'Không biết là Bae đang iu anh như anh iu bae không nhỉ?😙',
-    desc: 'Để thế giới này bớt đi 2 kẻ cô đơn, Bae cho anh một cơ hội để cùng Bae đi tiếp quảng đường còn lại nhé?',
+    btnIntro: 'Người đẹp chào lại nà',
+    title: 'Bae à, em đang iu anh như anh iu em không nhỉ?😙',
+    desc: 'Để thế giới này bớt đi 2 kẻ cô đơn, Bae cho anh một cơ hội để cùng Bae đi tiếp quãng đường còn lại nhé?',
     btnYes: 'Có, em rất đồng ý <3',
     btnNo: 'Không, đừng có mà mơ :3',
-    question:'Anh trộm nghĩ, điều gì làm cho em chịu về bên anh vậy ta?',
+    question:'Anh trộm nghĩ, điều gì làm cho em chịu ở bên anh vậy ta?',
     btnReply: 'Gửi cho anh này <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. hí hí 😘😘',
-    messDesc: 'Thế ăn ở bên anh cả đời nhé?',
+    messDesc: 'Thế ở bên anh cả đời nhé?',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/tai.duong601/' 
 }
