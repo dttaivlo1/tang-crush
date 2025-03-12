0f0a1d82cfe1398690f5a8c999b2d12494a13876
dttaivlo1 <76785450+dttaivlo1@users.noreply.github.com>
2025-03-12 15:33:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Hề Lô my Dược sĩ`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng dược sĩ đang đói nhỉ? 😙',
-    desc: 'Có muốn ăn quả sầu riêng 80 kí khongggg? ',
-btnYes: 'Sầu riêng mãi đỉnh <33',
+    introTitle: 'Kiều à!',
+    introDesc: `Chào người đẹp của anh nhé`,
+    btnIntro: 'He he hh hee',
+    title: 'Không biết là Bae đang iu anh như anh iu bae không nhỉ?😙',
+    desc: 'Để thế giới này bớt đi 2 kẻ cô đơn, Bae cho anh một cơ hội để cùng Bae đi tiếp quảng đường còn lại nhé',
+    btnYes: 'Có, em rất đồng ý <3',
     btnNo: 'Không, đừng có mà mơ :3',
-    question:'Thế sao mà Dược sĩ lại chọn sầu riêng thế <3',
-    btnReply: 'Gửi cho tớ này <3',
+    question:'Anh trộm nghĩ, điều gì làm cho em chịu về bên anh vậy ta?',
+    btnReply: 'Gửi cho anh này <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tui biết mà 🥰. hí hí 😘😘',
-    messDesc: 'Thế ăn sầu riêng cả đời nhé',
+    mess: 'Anh biết mà 🥰. hí hí 😘😘',
+    messDesc: 'Thế ăn ở bên anh cả đời nhé?',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/mot2ba4ngay/' 
+    messLink: 'https://m.me/tai.duong601/' 
 }
